948ad6ba0265e4416feb4dd97c4007a2f25f25a7
Bradley Martin <bam116@ic.ac.uk>
2021-12-09 21:34:51 +0000

    Update variation.jl

--- src/variation.jl
+++ src/variation.jl
@@ -92,7 +92,7 @@ multi_variation(T::Float64, ϵ_optic::Float64, m_eff::Float64, volume::Float64, 
      - initial_vw determines if the function should start with a random initial set of variational parameters (Bool input) or a given set of variational parameter values (one dimensional array).
      - N specifies the number of variational parameter pairs, v_p and w_p, to use in minimising the free energy.
 """
-function variation(α::Array, β::Array; v = 0.0, w = 0.0, ω = 1.0, N = 1, T = 10.0) # N number of v and w params
+function variation(α::Array, β::Array; v = 0.0, w = 0.0, ω = 1.0, N = 1, T = 20.0) # N number of v and w params
 
     if N != length(v) != length(w)
         return error("The number of variational parameters v & w must be equal to N.")
@@ -122,7 +122,7 @@ function variation(α::Array, β::Array; v = 0.0, w = 0.0, ω = 1.0, N = 1, T = 
 		lower,
 		upper,
 		initial,
-		Fminbox(BFGS()),
+		Fminbox(LBFGS()),
 		Optim.Options(time_limit = T), # Set time limit for asymptotic convergence if needed.
 	)
 
@@ -130,8 +130,8 @@ function variation(α::Array, β::Array; v = 0.0, w = 0.0, ω = 1.0, N = 1, T = 
 	var_params = Optim.minimizer(solution)
 
 	# Separate the v and w parameters into one-dimensional arrays (vectors).
-	v = var_params[1:2:2*N]
-	w = var_params[2:2:2*N]
+	v = var_params[2:2:2*N]
+	w = var_params[1:2:2*N]
 
 	# Print the variational parameters that minimised the free energy.
 	# println("Variational parameters: ", var_params)
@@ -140,7 +140,7 @@ function variation(α::Array, β::Array; v = 0.0, w = 0.0, ω = 1.0, N = 1, T = 
     return v, w
 end
 
-function variation(α::Array; v = 0.0, w = 0.0, ω = 1.0, N = 1, T = 10) # N number of v and w params
+function variation(α::Array; v = 0.0, w = 0.0, ω = 1.0, N = 1, T = 20) # N number of v and w params
  
     if N != length(v) != length(w)
         return error("The number of variational parameters v & w must be equal to N.")
@@ -170,7 +170,7 @@ function variation(α::Array; v = 0.0, w = 0.0, ω = 1.0, N = 1, T = 10) # N num
 		lower,
 		upper,
 		initial,
-		Fminbox(BFGS()),
+		Fminbox(LBFGS()),
 		Optim.Options(time_limit = T), # Set time limit for asymptotic convergence if needed.
 	)
 
@@ -178,8 +178,8 @@ function variation(α::Array; v = 0.0, w = 0.0, ω = 1.0, N = 1, T = 10) # N num
 	var_params = Optim.minimizer(solution)
 
 	# Separate the v and w parameters into one-dimensional arrays (vectors).
-	v = var_params[1:2:2*N]
-	w = var_params[2:2:2*N]
+	v = var_params[2:2:2*N]
+	w = var_params[1:2:2*N]
 
 	# Print the variational parameters that minimised the free energy.
 	# println("Variational parameters: ", var_params)
