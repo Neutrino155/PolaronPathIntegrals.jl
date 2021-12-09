6ebe532627298694bbc8abb1340d5b24ad12f184
Bradley Martin <bam116@ic.ac.uk>
2021-12-09 22:51:54 +0000

    Update variation.jl

--- src/variation.jl
+++ src/variation.jl
@@ -123,7 +123,7 @@ function variation(α::Array, β::Array; v = 0.0, w = 0.0, ω = 1.0, N = 1, T = 
 		upper,
 		initial,
 		SAMIN(),
-		Optim.Options(show_trace = true, show_every = 50), # Set time limit for asymptotic convergence if needed.
+		Optim.Options(iterations=10^6, show_trace = true, show_every = 50), # Set time limit for asymptotic convergence if needed.
 	)
 
 	# Extract the v and w parameters that minimised the free energy.
